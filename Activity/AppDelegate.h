38b414e5efb201b7ff29ff41543ad9a928d41613
ferrakkem <ferrakkem@gmail.com>
2015-04-27 00:28:35 +0600

    Initial Commit

---
+++ Activity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Activity
+//
+//  Created by Ferrakkem Bhuiyan on 27/04/2015.
+//  Copyright (c) 2015 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Activity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Activity
+//
+//  Created by Ferrakkem Bhuiyan on 27/04/2015.
+//  Copyright (c) 2015 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
